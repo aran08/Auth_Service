05475c83cc3138f8fb07f98701fdcc125c62812d
Aran Choudhary <aranjaish2308@gmail.com>
2023-07-28 13:45:51 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const bodyParser = require('body-parser');
 const {PORT} = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
 
-// const { User } = require('./models/index');
-// const bcrypt = require('bcrypt');
-// const UserRepository = require('./repository/user-repository');
+//const UserService= require('./services/user-service');
 
 const app = express();
 
@@ -19,13 +17,15 @@ const prepareAndStartService = () => {
 
     app.listen(PORT, async () => {
         console.log(`Server Started on port : ${PORT}`);
-        // const repo = new UserRepository();
-        // const response = await repo.getById(1);
-        // console.log(response);
-        // const incomingpassword = '123456';
-        // const hash = await User.findByPk(3);
-        // const response = bcrypt.compareSync(incomingpassword, user.password);
-        // console.log(response);
+        
+
+    // const service = new UserService();
+    // // const newToken = service.createToken({email:'aranjaish2308@gmail.com', id:1});
+    // // console.log(" New Token is : ", newToken);
+    // // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFyYW5qYWlzaDIzMDhAZ21haWwuY29tIiwiaWQiOjEsImlhdCI6MTY5MDUzMTUzMSwiZXhwIjoxNjkwNTM1MTMxfQ.r5eXpwl07Vkr_nxhQguwxim4tE9hd9zEg4x4RZIHAcU'
+    // // const respone = service.verifyToken(token);
+    // // console.log(respone);
+
     })
 }
 
